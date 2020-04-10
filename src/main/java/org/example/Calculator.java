6f15ae9320aa27c8f9d7323313b03af14455395f
The-Popa <tobias.carlstrom@bredband2.com>
2020-04-10 11:10:46 +0200

    Comments

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -7,20 +7,31 @@ public class Calculator
 {
     public static void main( String[] args )
     {
-        int num1 = 0;
-        int num2 = 0;
+       /* Declares inputs and outputs
+       double for input numbers
+       char for operators
+       double for outputs(answer)
+        */
+        double num1 = 0;
+        double num2 = 0;
         char operator;
         double answer = 0.0;
+
+        /* Imported Scanner object (top line and below in code)
+        for user input
+         */
 
         Scanner scanObject = new Scanner(System.in);
 
         System.out.println("Enter first number: ");
-        num1 = scanObject.nextInt();
+        num1 = scanObject.nextDouble();
         System.out.println("Enter second number: ");
-        num2 = scanObject.nextInt();
+        num2 = scanObject.nextDouble();
         System.out.println("Select operation: ");
         operator = scanObject.next().charAt(0);
 
+        /* Switch statement, receives operator variable input and choose a case
+         */
         switch (operator) {
             case '+': answer = num1 + num2;
             break;
@@ -31,6 +42,8 @@ public class Calculator
             case '/': answer = num1 / num2;
             break;
         }
+        /* Displays answer
+         */
         System.out.println(num1+" "+operator+" "+num2+" = "+answer);
     }
 }
